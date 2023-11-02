ec9cd4aa1edd298dcd0e5ef4f6924aee6ddda0d9
Julian Guzman <jhguzmans@unal.edu.co>
2023-11-02 12:14:13 -0500

    Back terminado con las relaciones del POST

--- server/src/controlers/c_postActivity.js
+++ server/src/controlers/c_postActivity.js
@@ -1,14 +1,43 @@
-const { Activity } = require("../db");
-const c_postActivity = async (name, dificult, duration, season) => {
-  const newActivity = {
-    name,
-    dificult,
-    duration,
-    season,
-  };
-  const createdActivity = await Activity.create(newActivity);
-  console.log(createdActivity);
-  console.log(createdActivity.proto);
-  return createdActivity;
+const { Activity, Country } = require("../db");
+const c_postActivity = async (
+  name,
+  dificult,
+  duration,
+  season,
+  countriesName
+) => {
+  try {
+    const newActivity = {
+      name,
+      dificult,
+      duration,
+      season,
+    };
+    const createdActivity = await Activity.create(newActivity);
+
+    if (countriesName && countriesName.length > 0) {
+      const createdCountries = [];
+      for (const countryName of countriesName) {
+        const existingCountry = await Country.findOne({
+          where: {
+            name: countryName,
+          },
+        });
+        if (existingCountry) {
+          createdCountries.push(existingCountry);
+        } else {
+          console.log(`El país ${countryName} no existe en la base de datos.`);
+        }
+      }
+      await createdActivity.addCountries(createdCountries);
+    }
+    const countries = await createdActivity.getCountries();
+    console.log(createdActivity);
+    createdActivity.dataValues.countries = countries;
+    return createdActivity;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
 module.exports = c_postActivity;
--- server/src/handlers/postActivities.js
+++ server/src/handlers/postActivities.js
@@ -2,12 +2,15 @@ const c_postActivity = require("../controlers/c_postActivity");
 const axios = require("axios");
 const postActivities = async (req, res) => {
   try {
-    const { name, dificult, duration, season } = req.body;
+    const { name, dificult, duration, season, countriesName } = req.body;
+    console.log("los datos que se envían en el handler son:");
+    console.log(countriesName);
     const createdActivity = await c_postActivity(
       name,
       dificult,
       duration,
-      season
+      season,
+      countriesName
     );
     return res.status(200).json(createdActivity);
   } catch (error) {
